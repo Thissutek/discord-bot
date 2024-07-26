56761e0166723fd3bcbd9160c35fc043aab0ab25
Jonathan Yau <jonathan_yau@outlook.com>
2024-07-25 22:47:04 -0400

    Added a cooldown for slash commands

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
 client.cooldowns = new Collection();
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
